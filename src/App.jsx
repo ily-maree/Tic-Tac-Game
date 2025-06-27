72a29b7befbf1e7729bc5e182fe460b97ea662ad
unknown <ily-maree>
2025-06-27 12:52:33 +0100

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import SelectPlayerPage from "./page/selectPlayerPage";
-import Game from "./page/Game";
+import Game from './page/game';
+// import GameVsCpu from './page/gameVsCpu'
 import { useState } from "react";
+import GameVsCpu from "./page/gameVsCpu";
 
 function App() {
     const [playerOneMark, setPlayerOneMark] = useState("X");
@@ -12,7 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<SelectPlayerPage setPlayerOneMark={setPlayerOneMark} playerOneMark={playerOneMark} setPlayerTwoMark={setPlayerTwoMark} playerTwoMark={playerTwoMark}/>}/>
          <Route path="/select-player" element={<SelectPlayerPage setPlayerOneMark={setPlayerOneMark} playerOneMark={playerOneMark} setPlayerTwoMark={setPlayerTwoMark} playerTwoMark={playerTwoMark}/>}/>
-        <Route path="/game" element={<Game setPlayerOneMark={setPlayerOneMark} playerOneMark={playerOneMark} setPlayerTwoMark={setPlayerTwoMark} playerTwoMark={playerTwoMark}/>}/>
+        <Route path="/game" element={<Game />}/>
+        <Route path="/GameVsCpu" element={<GameVsCpu />}/>
+
       </Routes>
       
 
